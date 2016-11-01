8b1943c03f82551a9917ec212708e296bef0cabd
thienkimlove <thienkimlove@gmail.com>
2016-11-02 00:02:54 +0700

    update

--- resources/views/admin/post/index.blade.php
+++ resources/views/admin/post/index.blade.php
@@ -54,6 +54,9 @@
                                         <button id-attr="{{$post->id}}" class="btn btn-primary btn-sm edit-post" type="button">Edit</button>&nbsp;
                                         {!! Form::open(['method' => 'DELETE', 'route' => ['admin.posts.destroy', $post->id]]) !!}
                                         <button type="submit" class="btn btn-danger btn-mini">Delete</button>
+										<button class="btn btn-primary btn-sm" type="button">
+                                        <a target="_blank" href="{{url($post->slug.'.html')}}" style="color:#FFFFFF">View Post</a>
+                                        </button>
                                         {!! Form::close() !!}
                                     </td>
                                 </tr>
--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -26,9 +26,9 @@
                         </div>
                     </div>
                 </div>
-                <div class="box-map">
-                    <img src="{{url('frontend/images/map.jpg')}}" width="1360" height="450" alt="">
-                </div>
+                <div class="boxMap">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14898.483493048278!2d105.8014624!3d21.007829299999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab5a02fbb0f5%3A0x75b5e966c9fb8bc0!2zQ8O0bmcgdHkgVE5ISCBUdeG7hyBMaW5o!5e0!3m2!1svi!2s!4v1441615369587" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
                 <!-- EndBoxContact -->
             </div><!--//col-left-->
             @include('frontend.right')
